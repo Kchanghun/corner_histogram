7b452f80121ac29ba4ec1f6b8053c0471293a721
Kchanghun <abcd877287@gmail.com>
2022-03-29 20:26:14 +0900

    put 2nd.jpg next to 1st.jpg

---
+++ corner_histogram.py
@@ -0,0 +1,27 @@
+import cv2
+from matplotlib import pyplot as plt
+
+path= './data/'
+img1st=cv2.imread(path+'1st.jpg')
+img2nd=cv2.imread(path+'2nd.jpg')
+#img1st=cv2.imread(path+'1st.jpg',cv2.IMREAD_GRAYSCALE)
+#img2nd=cv2.imread(path+'2nd.jpg',cv2.IMREAD_GRAYSCALE)
+
+img1st=cv2.cvtColor(img1st,cv2.COLOR_BGR2RGB)
+img2nd=cv2.cvtColor(img2nd,cv2.COLOR_BGR2RGB)
+#img1st=cv2.cvtColor(img1st,cv2.IMREAD_GRAYSCALE)
+#img2nd=cv2.cvtColor(img2nd,cv2.IMREAD_GRAYSCALE)
+
+fig, ax = plt.subplots(1,2,figsize=(10,5),sharey=True)
+
+ax[0].axis('off')
+ax[0].imshow(img1st,aspect='auto')
+
+ax[1].axis('off')
+ax[1].imshow(img2nd,aspect='auto')
+
+plt.subplots_adjust(left=0,bottom=0,right=1,top=1,wspace=0,hspace=0)
+
+plt.gcf().canvas.manager.set_window_title('Click Corner')
+plt.savefig('./data/putTogether.jpg',bbox_inches='tight')
+plt.show()
