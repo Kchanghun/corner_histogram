ff59afef50687be1fd1e727edd01e4254915f3b2
Kchanghun <abcd877287@gmail.com>
2022-03-29 22:34:21 +0900

    get 8 ROI from Mouse event

--- corner_histogram.py
+++ corner_histogram.py
@@ -1,27 +1,36 @@
 import cv2
-from matplotlib import pyplot as plt
+import numpy as np
 
-path= './data/'
-img1st=cv2.imread(path+'1st.jpg')
-img2nd=cv2.imread(path+'2nd.jpg')
-#img1st=cv2.imread(path+'1st.jpg',cv2.IMREAD_GRAYSCALE)
-#img2nd=cv2.imread(path+'2nd.jpg',cv2.IMREAD_GRAYSCALE)
 
-img1st=cv2.cvtColor(img1st,cv2.COLOR_BGR2RGB)
-img2nd=cv2.cvtColor(img2nd,cv2.COLOR_BGR2RGB)
-#img1st=cv2.cvtColor(img1st,cv2.IMREAD_GRAYSCALE)
-#img2nd=cv2.cvtColor(img2nd,cv2.IMREAD_GRAYSCALE)
+# img = cv2.imread('./data/putTogether_Color.jpg')
+img = cv2.imread('./data/putTogether_Gray.jpg')
+# (x, y, )
+roi_1st = []
+roi_2nd = []
 
-fig, ax = plt.subplots(1,2,figsize=(10,5),sharey=True)
+def onMouse(event, x, y, flags, param):
+    global roi_1st, roi_2nd
+    if event == cv2.EVENT_LBUTTONDOWN:
+        #left down --> 1st image
+        roi_1st.append([x, y])
+        # patch size is 9
+        cv2.rectangle(param[0], (x - 4, y - 4),
+                                (x + 5, y + 5), (0, 0, 255), 2)    
+        # print('roi_1st ',roi_1st)
+        # print('roi_2nd ',roi_2nd)
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        #left down --> 1st image
+        roi_2nd.append([x, y])
+        # patch size is 9
+        cv2.rectangle(param[0], (x - 4, y - 4),
+                                (x + 5, y + 5), (0, 255, 0), 2)
+        # print('roi_1st ',roi_1st)
+        # print('roi_2nd ',roi_2nd)
+    
+    cv2.imshow('Click center of ROI',param[0])
+            
 
-ax[0].axis('off')
-ax[0].imshow(img1st,aspect='auto')
-
-ax[1].axis('off')
-ax[1].imshow(img2nd,aspect='auto')
-
-plt.subplots_adjust(left=0,bottom=0,right=1,top=1,wspace=0,hspace=0)
-
-plt.gcf().canvas.manager.set_window_title('Click Corner')
-plt.savefig('./data/putTogether.jpg',bbox_inches='tight')
-plt.show()
+cv2.imshow('Click center of ROI',img)
+cv2.setMouseCallback('Click center of ROI',onMouse,[img])
+cv2.waitKey()
+cv2.destroyAllWindows()
