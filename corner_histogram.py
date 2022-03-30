0946d7330c9d9acf4950d4a1715ed485025ff46b
Kchanghun <abcd877287@gmail.com>
2022-03-30 10:40:22 +0900

    can see first ROI's histogram but not gradient

--- corner_histogram.py
+++ corner_histogram.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 #img = cv2.imread('./data/putTogether_Color.jpg')
 img = cv2.imread('./data/putTogether_Gray.jpg',cv2.IMREAD_GRAYSCALE)
@@ -10,11 +10,11 @@ font=cv2.FONT_HERSHEY_SIMPLEX
 roi_1st_origin = []
 roi_2nd_origin = []
 
-patch_size = 9
-
 # Roi Image
 roi_1st_IM = []
 roi_2nd_IM = []
+
+patch_size = 9
 
 def onMouse(event, x, y, flags, param):
     global roi_1st_origin, roi_2nd_origin
@@ -32,7 +32,6 @@ def onMouse(event, x, y, flags, param):
             cv2.putText(img,str(index),roi_1st_origin[index-1],font,1,255,2)
             cv2.rectangle(param[0], (x - patch_size // 2, y - patch_size // 2),
                           (x + patch_size // 2, y + patch_size // 2), (0, 0, 255), 2)
-            # cv2.imshow('Click center of ROI',param[0])
             # print('roi_1st_origin ',roi_1st_origin)
     elif event == cv2.EVENT_RBUTTONDOWN:
         # right down --> 2nd image
@@ -44,7 +43,7 @@ def onMouse(event, x, y, flags, param):
                       (x + patch_size // 2, y + patch_size // 2), (0, 255, 0), 2)
         # print('roi_2nd_origin ',roi_2nd_origin)
     
-    cv2.imshow('Click center of ROI',param[0])
+    cv2.imshow('Match ROI',param[0])
 
 def getRoiImage(which, origin):
     global img, roi_1st_IM, roi_2nd_IM
@@ -56,10 +55,23 @@ def getRoiImage(which, origin):
         roi_2nd_IM.append(img[origin[0]:origin[0]+patch_size,
                               origin[1]:origin[1]+patch_size])
         # cv2.imshow('2nd Rois '+str(roi_1st_IM.__len__()),roi_1st_IM[roi_1st_IM.__len__()-1])
+        
+    if roi_2nd_IM.__len__() == 4:
+        drawHist()
     
+def drawHist():
+    global roi_1st_IM, roi_2nd_IM
+    index = 0
+    roi_1st_hist = []
     
+    roi_1st_hist.append(cv2.calcHist(images=[roi_1st_IM[index]],channels=[0],mask=None,
+                                     histSize=[256], ranges=[0,256]))
+    roi_1st_hist[index] = roi_1st_hist[index].flatten()
+    binX = np.arange(256)
+    plt.bar(binX,roi_1st_hist[index],width=1,color = 'b')
+    plt.show()
 
-cv2.imshow('Click center of ROI',img)
-cv2.setMouseCallback('Click center of ROI',onMouse,[img])
+cv2.imshow('Match ROI',img)
+cv2.setMouseCallback('Match ROI',onMouse,[img])
 cv2.waitKey()
 cv2.destroyAllWindows()
